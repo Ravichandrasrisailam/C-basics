0b0a0a9ec554c3278e91d5348ff44fd088f67961
Ravichandrasrisailam <92910262+Ravichandrasrisailam@users.noreply.github.com>
2021-10-28 23:59:40 -0700

    Add files via upload

---
+++ Factor speciifier.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//variable declartion
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initailization 
+	age= 17;
+	cgpa= 8.8;
+	initial="m";
+	
+	//output
+	
+	printf("age of the student is %d\n", age);
+	printf("cgpa of the student is %f\n", cgpa);
+	printf("surname of the student is %c\n", initial);
+}
