641781ecd1f721dbe61db1d4d350fd4e30851847
Ravichandrasrisailam <92910262+Ravichandrasrisailam@users.noreply.github.com>
2021-10-30 00:03:10 -0700

    Add files via upload

---
+++ caluclations.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	//variable
+	int num1,num2,num3, sum;
+	
+	//intialization
+	num1=10;
+	num2=20;
+	num3=40;
+
+    //calculation
+   sum = num1+num2+num3;
+   
+   //output
+   printf("Sum of %d ,%d and %d is %d",num1,num2,num3,sum);
+}
