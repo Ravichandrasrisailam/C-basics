211761857ab31fdc3014c4fcc83c555f40f7cf01
Ravichandrasrisailam <92910262+Ravichandrasrisailam@users.noreply.github.com>
2021-11-06 00:04:59 -0700

    Add files via upload

---
+++ C Arithmetic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, s1,s2,s3,s4,s5;
+	 a=100;
+	 b=20;
+	 
+	 s1= a+b;
+	 s2= a*b;
+	 s3= a/b;
+	 s4= a-b;
+	 s5= a%b;
+	 	 printf("\n sum of %d and %d is %d",a,b,s1 );
+	 	 printf("\n mul of %d and %d is %d",a,b,s2);
+	 	 printf("\n div of %d and %d is %d",a,b,s3);
+	 	 printf("\n sub of %d and %d is %d",a, b,s4);
+	 	 printf("\n mod of %d and %d is %d ",a,b,s5);
+	 }
+
