0281d0b110a0585e3a6b8870c1e95ee20afaa8f5
Ravichandrasrisailam <92910262+Ravichandrasrisailam@users.noreply.github.com>
2021-11-11 12:33:33 +0530

    Add files via upload

---
+++ scanf  C.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("enter 4 numbers");
+	scanf("%d%d%d%d",&a, &b, &c, &d);
+	printf("%d",a*b*c*d);
+}
---
+++ scanf problrms C.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int l,w;
+	printf("\n enter lengnth of rectangle ");
+	scanf("%d", &l);
+	printf("\n enter width of rectangle");
+	scanf("%d", &w);
+	printf("\n area of rectangle   %d",l*w);
+	
+	printf("\n perimeter of rectangle %d",2*(l+w));
+	
+}
