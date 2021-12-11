8ec8de96d28be57d01d06dda7e5521e437dd2dc6
Ravichandrasrisailam <92910262+Ravichandrasrisailam@users.noreply.github.com>
2021-12-10 22:50:30 -0800

    Add files via upload

---
+++ c prog 2 .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int number;
+ printf("enter a number: ");
+ scanf("%d",&number);
+ if(number>0)
+ printf("its a positive ");
+ else
+ printf("its a negative");
+}
---
+++ c prog 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("enter s number: ");
+	scanf("%d",&number);
+	if (number%2==0)
+	printf("its a even number");
+	else
+	printf("its a odd number");
+	
+}
