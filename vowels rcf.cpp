78924ba4e08689413f708e18a490a553dd26c5eb
Ravichandrasrisailam <92910262+Ravichandrasrisailam@users.noreply.github.com>
2022-03-17 12:03:09 +0500

    Add files via upload

---
+++ vowels rcf.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    switch(ch)
+    {
+    	printf("consonant")
+    	break;
+   case 'A':
+   case 'E';
+   case 'I'
+   case 'O';
+   case 'U';
+    	printf ("vowel");
+    	break ;
+    \default:	
+
+	    
+    }
+}
+
